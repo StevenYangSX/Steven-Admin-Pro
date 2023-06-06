a62a66207ef12659f2ee8fad77a4aeb7398c2fe0
Steven Yang <yangshixin0629@qq.com>
2023-06-06 16:11:53 -0700

    Initialzed backend services project

---
+++ service/stevenadminservices/common/src/test/java/com/steven/stevenadmin/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steven.stevenadmin.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
