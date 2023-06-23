3b6894d139a5341c05873f14511ca124b1f0cf6e
Steven Yang <yangshixin0629@qq.com>
2023-06-23 16:51:51 -0700

    Added some dependencies.

--- client/steven-admin-client/src/App.tsx
+++ client/steven-admin-client/src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { green } from "@mui/material/colors";
+import Icon from "@mui/material/Icon";
+import "./App.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained">Hello World</Button>
+      <Box
+        sx={{
+          "& > :not(style)": {
+            m: 2,
+          },
+        }}
+      >
+        <Icon>add_circle</Icon>
+        <Icon color="primary">add_circle</Icon>
+        <Icon sx={{ color: green[500] }}>add_circle</Icon>
+        <Icon fontSize="small">add_circle</Icon>
+        <Icon sx={{ fontSize: 30 }}>add_circle</Icon>
+      </Box>
+      <Icon>star</Icon>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <Alert severity="error">This is an error alert — check it out!</Alert>
+        <Alert severity="warning">This is a warning alert — check it out!</Alert>
+        <Alert severity="info">This is an info alert — check it out!</Alert>
+        <Alert severity="success">This is a success alert — check it out!</Alert>
+      </Stack>
     </div>
   );
 }
